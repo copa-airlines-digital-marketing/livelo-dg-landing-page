82cf040accecc82cb18f9be2847b7a50c06fa9df
josevch <javch1995@gmail.com>
2024-08-06 11:49:14 -0500

    Init for livelo lp assets

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<div class="w-full bg-primary min-h-[256px] grid grid-rows-[64px,auto,64px]">
+    <div class="row-start-2">
+    <div class="container mx-auto"><nav aria-label="Breadcrumb" class="!mb-24">
+    <ol class="Breadcrumb flex gap-8 md:gap-16 items-center">
+    <li class="text-grey-75 font-sans hover:underline active:underline text "><a href="https://www.copaair.com/pt">Inicio</a></li>
+    <li class="text-grey-75 font-sans">&rsaquo;</li>
+    <li class="text-grey-75 font-sans hover:underline active:underline"><a href="promocoes">Promo&ccedil;&otilde;es</a></li>
+    </ol>
+    </nav>
+    <h1 class="font-heading font-heading-bold text-grey-75 text-h1-sm lg:text-h1-lg md:text-h1-md mb-8">Livelo</h1>
+    <h2 class=" font-sans text-18/24 text-grey-100 ">Ao transferir seus pontos Livelo para o ConnectMiles, voc&ecirc; pode aproveitar mais de 85 destinos diferentes com a Copa Airlines em 32 pa&iacute;ses diferentes nas Am&eacute;ricas e viver momentos incr&iacute;veis!</h2>
+    </div>
+    </div>
+    </div>
+
+    <div class="container mx-auto !my-16 flex flex-col items-center">
+        <h3 class="font-sans medium text-30-32 text-primary-main tracking-r003 text-center font-medium text-grey-8">Veja mais ofertas de voos com ConnectMiles</h3>
+            <a href="#">
+                <button class="bg-primary border border-primary font-medium gap-8 inline-flex items-center px-8 py-4 relative rounded-full text-grey-50 m-4  ">Reserve ja</button>
+            </a>
+    </div>
